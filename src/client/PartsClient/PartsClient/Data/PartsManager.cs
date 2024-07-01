039929c2fc15b7032668f65dc4300801a59c092e
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2024-06-30 20:21:49 -0600

    Create the HTTP Methods to CRUD.

--- src/client/PartsClient/PartsClient/Data/PartsManager.cs
+++ src/client/PartsClient/PartsClient/Data/PartsManager.cs
@@ -1,43 +1,85 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Json;
-using System.Text;
+﻿using System.Net.Http.Json;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace PartsClient.Data;
 
 public static class PartsManager
 {
-    // TODO: Add fields for BaseAddress, Url, and authorizationKey
-    static readonly string BaseAddress = "URL GOES HERE";
-    static readonly string Url = $"{BaseAddress}/api/";
+    private const string BaseAddress = "https://mslearnpartsserver70045167.azurewebsites.net";
+    private const string Url = $"{BaseAddress}/api";
+    private static string _authorizationKey;
+    private static readonly HttpClient Client = new();
 
-    static HttpClient client;
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new()
+    {
+        PropertyNameCaseInsensitive = true
+    };
 
     private static async Task<HttpClient> GetClient()
     {
-        throw new NotImplementedException();
+        if (!string.IsNullOrEmpty(_authorizationKey))
+            return Client;
+
+        _authorizationKey = await Client.GetStringAsync($"{Url}/login");
+        _authorizationKey = JsonSerializer.Deserialize<string>(_authorizationKey);
+
+        Client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _authorizationKey);
+        Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+        return Client;
     }
 
     public static async Task<IEnumerable<Part>> GetAll()
     {
-        throw new NotImplementedException();                
+        if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            return [];
+
+        var client = await GetClient();
+        var result = await client.GetStringAsync($"{Url}/parts");
+
+        return JsonSerializer.Deserialize<IEnumerable<Part>>(result, JsonSerializerOptions) ?? [];
     }
 
-    public static async Task<Part> Add(string partName, string supplier, string partType)
+    public static async Task Add(string partName, string supplier, string partType)
     {
-        throw new NotImplementedException();
+        if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet) return;
+
+        var part = new Part
+        {
+            PartName = partName,
+            Suppliers = [.. new[] { supplier }],
+            PartID = string.Empty,
+            PartType = partType,
+            PartAvailableDate = DateTime.Now.Date
+        };
+
+        var client = await GetClient();
+
+        var response = await client.PostAsJsonAsync($"{Url}/parts", part);
+        response.EnsureSuccessStatusCode();
+
+        await response.Content.ReadFromJsonAsync<Part>(JsonSerializerOptions);
     }
 
     public static async Task Update(Part part)
     {
-        throw new NotImplementedException();
+        if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            return;
+
+        var client = await GetClient();
+
+        var response = await client.PutAsJsonAsync($"{Url}/parts/{part.PartID}", part);
+        response.EnsureSuccessStatusCode();
     }
 
-    public static async Task Delete(string partID)
+    public static async Task Delete(string partId)
     {
-        throw new NotImplementedException();                        
+        if (Connectivity.Current.NetworkAccess != NetworkAccess.Internet)
+            return;
+
+        var client = await GetClient();
+
+        var response = await client.DeleteAsync($"{Url}/parts/{partId}");
+        response.EnsureSuccessStatusCode();
     }
 }
